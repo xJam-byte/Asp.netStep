1f65688e8acafaa5c4aee8aac8137818527b7c44
xJam-byte <abylaimuratbayev@gmail.com>
2023-08-14 20:52:01 +0600

    Добавьте файлы проекта.

---
+++ WebApplication2/Views/Rooms/index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Views.Rooms
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
